6bf64a399449f4355249cc1625179857ec5ef47c
Ross Duncan <ross.hugh.duncan@gmail.com>
2020-07-05 16:38:53 +0100

    Pages are together - part 1

---
+++ build/precache-manifest.2678bdb6f2d7c452b52a40d770b547f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f548cbecb463a9abac5ca3b88f98e87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6031fb9b34773cd3f196",
+    "url": "/static/css/main.836d98a2.chunk.css"
+  },
+  {
+    "revision": "db69ea19ea359bfa66f1",
+    "url": "/static/js/2.3b5b58cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3b5b58cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6031fb9b34773cd3f196",
+    "url": "/static/js/main.d7c5ca84.chunk.js"
+  },
+  {
+    "revision": "fd54790c8d56e5648eec",
+    "url": "/static/js/runtime-main.02b12b87.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import styles from './styling/App.module.css';
+import { pubImg } from './global/globalFunctions';
+import HeaderBar from './navigation/HeaderBar';
+import SectionBar from './navigation/SectionBar';
+import MainBodyController from './main_body_comps/MainBodyController';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    /********************
+     * Set home page here
+     *******************/
+    headerBarSelection: 'Home',
+    sectionBarSelection: 'Welcome'
+  };
+
+  headerBarClickHandler = (tab) => {
+    this.setState({ headerBarSelection: tab });
+
+    /********************************************** 
+    * Set default section selection for each header
+    ***********************************************/
+    if (tab === 'Home') {
+      this.setState({ sectionBarSelection: 'Welcome' });
+    }
+    // On Portfolio tab
+    else {
+      this.setState({ sectionBarSelection: 'Audio Production' });
+    }
+  };
+
+  sectionBarClickHandler = (tab) => {
+    this.setState({ sectionBarSelection: tab});
+  };
+
+  render() {
+    return (
+      <div className={styles.App}>
+        <img
+          src={pubImg('Logo.png')}
+          alt="Logo"
+          width='240px'
+          height='36px'
+        />
+        <HeaderBar
+          buttonClicked={this.headerBarClickHandler}
+          selectedItem={this.state.headerBarSelection}
+        />
+        <SectionBar
+          buttonClicked={this.sectionBarClickHandler}
+          selectedHeader={this.state.headerBarSelection}
+          selectedSection={this.state.sectionBarSelection}
+        />
+        <MainBodyController
+          selectedHeader={this.state.headerBarSelection}
+          selectedSection={this.state.sectionBarSelection}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
